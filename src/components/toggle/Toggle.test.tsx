9803b16549a452e0084b924049748b346cebc9bf
cristian.munozm <cristian.munozm@perficient.com>
2024-05-03 16:33:09 -0500

    Update tests

--- src/components/toggle/Toggle.test.tsx
+++ src/components/toggle/Toggle.test.tsx
@@ -1,30 +1,16 @@
-import { vi } from 'vitest';
 import { render, screen } from '@testing-library/react';
-import React from 'react';
 import Toggle from './Toggle';
-import { themeContext } from '../../infrastructure/themeContext';
 
 describe('Toggle', () => {
   test('Should render toggle component', () => {
-    render(<Toggle />);
+    render(<Toggle theme={{ isDark: false }} handleClick={() => {}} />);
 
     expect(screen.getByText('Light Mode')).toBeDefined();
     expect(screen.getByRole('checkbox')).toBeDefined();
   });
 
   test('should render the component with dark theme', () => {
-    const mockContextValue = {
-      theme: { isDark: true },
-      handleTheme: vi.fn(),
-      handleList: vi.fn(),
-    };
-    vi.spyOn(React, 'useContext').mockReturnValue({ mockContextValue });
-
-    render(
-      <themeContext.Provider value={mockContextValue}>
-        <Toggle />
-      </themeContext.Provider>
-    );
+    render(<Toggle theme={{ isDark: true }} handleClick={() => {}} />);
 
     expect(screen.getByText('Dark Mode')).toBeDefined();
   });
