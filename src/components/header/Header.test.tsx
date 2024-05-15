f8a814427c78d179dfb48ae1a763ce22ef9af36a
cristian.munozm <cristian.munozm@perficient.com>
2024-05-15 15:20:07 -0500

    update tests

--- src/components/header/Header.test.tsx
+++ src/components/header/Header.test.tsx
@@ -4,7 +4,11 @@ import { render, screen } from '@testing-library/react';
 
 describe('Header', () => {
   test('Should render Header component', () => {
-    render(<Header title='Title Header' toggle={<Toggle />} />);
+    render(
+      <Header title='Title Header'>
+        <Toggle theme={{ isDark: false }} handleClick={() => {}} />
+      </Header>
+    );
 
     expect(screen.getByRole('link')).toBeDefined();
     expect(screen.getByText('Title Header')).toBeDefined();
